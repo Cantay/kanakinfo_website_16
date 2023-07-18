f08f810748ef7fc89a5edc7e28f66fe9f4eeefeb
Patel Dilip <dka@kanakinfosystems.com>
2023-07-18 17:59:34 +0530

    [IMP]modify code git connector

--- kanak_github_connector/models/github_repository.py
+++ kanak_github_connector/models/github_repository.py
@@ -11,13 +11,19 @@ import re
 import lxml.html
 from os.path import join as opj
 from odoo import fields, models, tools
-from odoo.modules.module import MANIFEST_NAMES
 
 _logger = logging.getLogger(__name__)
 try:
     from git import Repo
 except ImportError:
     _logger.debug("Cannot import 'git' python library.")
+
+MANIFEST_NAMES = ['__openerp__.py', '__manifest__.py']
+_ICON_PATH = [
+    'static/src/img/',
+    'static/description/',
+]
+_INDEX_HTML_PATH = 'static/description/'
 
 
 class GithubRepository(models.Model):
@@ -28,133 +34,119 @@ class GithubRepository(models.Model):
     github_url = fields.Char(string='Github URL', required=True)
 
     def action_branch_clone(self):
-        owner_match = re.search(r'github\.com:(.*)/', self.github_url)
-        owner = owner_match.group(1)
-        get_param = self.env['ir.config_parameter'].sudo().get_param
-        token = get_param('git_token')
-        branch_match = re.search(r'#(.*)', self.github_url)
-        branch = branch_match.group(1)
-        repo_match = re.search(r'github\.com:.*/(.*).git', self.github_url)
-        repo = repo_match.group(1)
+        owner = re.search(r'github\.com:(.*)/', self.github_url).group(1)
+        token = self.env['ir.config_parameter'].sudo().get_param('git_token')
+        branch = re.search(r'#(.*)', self.github_url).group(1)
+        repo = re.search(r'github\.com:.*/(.*).git', self.github_url).group(1)
         repo_url = f'https://{token}@github.com/{owner}/{repo}.git'
         product_product_obj = self.env['product.product'].sudo()
+        
         try:
             temp_dir = tempfile.mkdtemp(prefix=f"kanak_apps_{branch}_")
             repo = git.Repo.clone_from(repo_url, temp_dir, branch=branch)
             contents = os.listdir(temp_dir)
+            
             for item in contents:
-                item_path = os.path.join(temp_dir, item)  # Get the full path of the item
+                item_path = os.path.join(temp_dir, item)
                 if os.path.isdir(item_path) and not item.startswith('.'):
-                    full_module_path = os.path.join(temp_dir, item)
                     module_info = {}
-                    _ICON_PATH = [
-                        'static/src/img/',
-                        'static/description/',
-                    ]
-                    _INDEX_HTML_PATH = 'static/description/'
+                    full_module_path = os.path.join(temp_dir, item)
                     manifest_path = next((opj(full_module_path, name) for name in MANIFEST_NAMES if os.path.exists(opj(full_module_path, name))), None)
-                    if manifest_path:
-                        if os.path.isfile(manifest_path):
-                            with open(manifest_path, 'rb') as f:
-                                module_info.update(ast.literal_eval(f.read().decode()))
-                        module_info['technical_name'] = item
-                        module_version = product_product_obj.create_or_update_variants(module_info, branch)
-                        theme_image_path = False
-                        banner_path = False
-                        icon_path = False
-                        resize = False
-                        product_image = False
-                        theme_image = False
-                        banner_image = False
-                        license = False
-                        if module_info.get('images', False):
-                            full_current_icon_path = os.path.join(
-                                full_module_path, module_info.get('images')[0])
-                            if os.path.exists(full_current_icon_path):
-                                banner_path = full_current_icon_path
-                            if module_info.get('images', False) and len(module_info.get('images', False)) > 1:
-                                full_current_theme_image_path = os.path.join(full_module_path, module_info.get('images', False)[1])
-                                if os.path.exists(full_current_theme_image_path):
-                                    theme_image_path = full_current_theme_image_path
-                        for current_icon_path in _ICON_PATH:
-                            full_current_icon_path = os.path.join(
-                                full_module_path, current_icon_path, 'icon.png')
-                            if os.path.exists(full_current_icon_path):
-                                icon_path = full_current_icon_path
-                                resize = True
-                                break
-                        if theme_image_path:
-                            image_enc = False
-                            try:
-                                with open(theme_image_path, 'rb') as f:
-                                    image = f.read()
-                                image_enc = base64.b64encode(tools.image_process(image, output_format='gif'))
-                            except Exception:
-                                _logger.warning("Unable to read or resize %s", banner_path)
-                            theme_image = image_enc
-                        if banner_path:
-                            image_enc = False
-                            try:
-                                with open(banner_path, 'rb') as f:
-                                    image = f.read()
-                                image_enc = base64.b64encode(tools.image_process(image, output_format='gif'))
-                            except Exception:
-                                _logger.warning("Unable to read or resize %s", banner_path)
-                            banner_image = image_enc
-                        if icon_path:
-                            image_enc = False
-                            try:
-                                with open(icon_path, 'rb') as f:
-                                    image = f.read()
-                                if resize:
-                                    image_enc = base64.b64encode(tools.image_process(image, output_format='png'))
-                                else:
-                                    image_enc = base64.b64encode(image)
-                            except Exception:
-                                _logger.warning("Unable to read or resize %s", icon_path)
-                            product_image = image_enc
-                        else:
-                            # Set the default icon
-                            try:
-                                with open(os.path.join(
-                                        os.path.dirname(__file__),
-                                        '../data/kanak.png'), 'rb') as f:
-                                    image = base64.b64encode(f.read())
-                                    product_image = image
-                            except Exception as e:
-                                _logger.error(
-                                    'Unable to read the icon image, error is %s', e)
-
-                        index_path = False
-                        description_rst_html = False
-                        full_current_index_path = os.path.join(full_module_path, _INDEX_HTML_PATH, 'index.html')
-                        license_path = os.path.join(full_module_path, 'LICENSE')
-                        if os.path.exists(full_current_index_path):
-                            index_path = full_current_index_path
-                        if index_path:
-                            with open(index_path, 'rb') as desc_file:
-                                doc = desc_file.read()
-                                html = lxml.html.document_fromstring(doc)
-                                for element, attribute, link, pos in html.iterlinks():
-                                    if element.get('src') and not '//' in element.get('src') and not 'static/' in element.get('src'):
-                                        element.set('src', "//apps.odoocdn.com/apps/assets/%s/%s/%s" % (branch, module_info['technical_name'], element.get('src')))
-                                description_rst_html = tools.html_sanitize(lxml.html.tostring(html))
-                        else:
-                            description_rst_html = module_info['description']
-
-                        if os.path.exists(license_path):
-                            with open(license_path, 'rb') as desc_file:
-                                license = desc_file.read()
-                        module_version.write({
-                            'repository_id': self.id,
-                            'version': branch,
-                            'description_rst_html': description_rst_html,
-                            'image_1920': product_image,
-                            'banner_image': banner_image,
-                            'theme_image': theme_image,
-                            'description_sale': module_info.get('summary', ''),
-                            'license': license or ''
-                        })
+                    
+                    if manifest_path and os.path.isfile(manifest_path):
+                        with open(manifest_path, 'rb') as f:
+                            module_info.update(ast.literal_eval(f.read().decode()))
+                    
+                    module_info['technical_name'] = item
+                    module_version = product_product_obj.create_or_update_variants(module_info, branch)
+                    
+                    for current_icon_path in _ICON_PATH:
+                        full_current_icon_path = os.path.join(full_module_path, current_icon_path, 'icon.png')
+                        if os.path.exists(full_current_icon_path):
+                            icon_path = full_current_icon_path
+                            resize = True
+                            break
+                    
+                    index_path = os.path.join(full_module_path, _INDEX_HTML_PATH, 'index.html')
+                    license_path = os.path.join(full_module_path, 'LICENSE')
+                    banner_path = None
+                    theme_image_path = None
+                    license = ''
+                    
+                    if module_info.get('images'):
+                        full_current_banner_path = os.path.join(full_module_path, module_info['images'][0])
+                        if os.path.exists(full_current_banner_path):
+                            banner_path = full_current_banner_path
+                        
+                        if len(module_info['images']) > 1:
+                            full_current_theme_image_path = os.path.join(full_module_path, module_info['images'][1])
+                            if os.path.exists(full_current_theme_image_path):
+                                theme_image_path = full_current_theme_image_path
+                    
+                    if os.path.exists(license_path):
+                        with open(license_path, 'rb') as desc_file:
+                            license = desc_file.read()
+                    
+                    description_rst_html = ''
+                    if os.path.exists(index_path):
+                        with open(index_path, 'rb') as desc_file:
+                            doc = desc_file.read()
+                            html = lxml.html.document_fromstring(doc)
+                            for element, attribute, link, pos in html.iterlinks():
+                                if element.get('src') and not '//' in element.get('src') and not 'static/' in element.get('src'):
+                                    element.set('src', "//apps.odoocdn.com/apps/assets/%s/%s/%s" % (branch, module_info['technical_name'], element.get('src')))
+                            description_rst_html = tools.html_sanitize(lxml.html.tostring(html))
+                    else:
+                        description_rst_html = module_info.get('description', '')
+                    
+                    product_image = None
+                    if icon_path:
+                        try:
+                            with open(icon_path, 'rb') as f:
+                                image = f.read()
+                            if resize:
+                                image = base64.b64encode(tools.image_process(image, output_format='png'))
+                            else:
+                                image = base64.b64encode(image)
+                            product_image = image
+                        except Exception:
+                            _logger.warning("Unable to read or resize %s", icon_path)
+                    else:
+                        try:
+                            with open(os.path.join(os.path.dirname(__file__), '../data/kanak.png'), 'rb') as f:
+                                image = base64.b64encode(f.read())
+                                product_image = image
+                        except Exception as e:
+                            _logger.error('Unable to read the icon image, error is %s', e)
+                    
+                    banner_image = None
+                    if banner_path:
+                        try:
+                            with open(banner_path, 'rb') as f:
+                                image = f.read()
+                            banner_image = base64.b64encode(tools.image_process(image, output_format='gif'))
+                        except Exception:
+                            _logger.warning("Unable to read or resize %s", banner_path)
+                    
+                    theme_image = None
+                    if theme_image_path:
+                        try:
+                            with open(theme_image_path, 'rb') as f:
+                                image = f.read()
+                            theme_image = base64.b64encode(tools.image_process(image, output_format='gif'))
+                        except Exception:
+                            _logger.warning("Unable to read or resize %s", theme_image_path)
+                    
+                    module_version.write({
+                        'repository_id': self.id,
+                        'version': branch,
+                        'description_rst_html': description_rst_html,
+                        'image_1920': product_image,
+                        'banner_image': banner_image,
+                        'theme_image': theme_image,
+                        'description_sale': module_info.get('summary', ''),
+                        'license': license
+                    })
 
             shutil.rmtree(temp_dir)
         except Exception as e:

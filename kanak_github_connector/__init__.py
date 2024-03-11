61bb5e9206097c2ac53d0679c8b84545f5dd0d92
Cantay <cantay@gmail.com>
2024-03-12 00:18:55 +0300

    improve

--- kanak_github_connector/__init__.py
+++ kanak_github_connector/__init__.py
@@ -1,3 +1,13 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from . import models
+# Import the models module from the django_app application
+from django_app.models import MyModel
+
+# Use the MyModel class from the models module
+m = MyModel(name="My Model", description="This is an example model")
+m.save()
+
+# Print the object's name and id
+print(f"Model name: {m.name}, ID: {m.id}")
+

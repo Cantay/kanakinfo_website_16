45bc12ccd41ed085b3132c2f0361042b7e3eec69
Cantay <cantay@gmail.com>
2024-03-12 00:18:57 +0300

    improve

--- kanak_website_erpnext/static/src/js/erp_website.js
+++ kanak_website_erpnext/static/src/js/erp_website.js
@@ -1,34 +1,44 @@
 odoo.define('kanak_website_erpnext.erp_website', function(require) {
     "use strict";
+
+    // Import the web dom ready module
     require('web.dom_ready');
 
+    // Import the jQuery library
+    const $ = require('jquery');
 
-    $('.erpnext-review .owl-carousel').owlCarousel({
-        center: true,
-        items: 1,
-        loop: true,
-        margin: 10,
-        nav: true,
-        autoplay: true,
-        autoplayTimeout: 3000,
-        responsive: {
-            0: {
-                items: 1
-            },
-            600: {
-                items: 1
-            },
-            1000: {
-                items: 3
-            },
-            1200: {
-                items: 3
+    // Check if the owlCarousel method is available
+    if ($.fn.owlCarousel) {
+        // Initialize the owlCarousel method on the selected elements
+        $('.erpnext-review .owl-carousel').owlCarousel({
+            center: true,
+            items: 1,
+            loop: true,
+            margin: 10,
+            nav: true,
+            autoplay: true,
+            autoplayTimeout: 3000,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: 1
+                },
+                1000: {
+                    items: 3
+                },
+                1200: {
+                    items: 3
+                }
             }
+        });
+    } else {
+        console.error('The owlCarousel method is not available');
+    }
 
-        }
-    });
-
-    $(".erpnext-review .owl-carousel .owl-prev").html('');
-    $(".erpnext-review .owl-carousel .owl-next").html('');
+    // Leave the prev and next buttons empty
+    // $(".erpnext-review .owl-carousel .owl-prev").html('');
+    // $(".erpnext-review .owl-carousel .owl-next").html('');
 
 });

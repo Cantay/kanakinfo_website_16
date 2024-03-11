14f352289d31cb71563c997c3bfac8d81184eec4
Cantay <cantay@gmail.com>
2024-03-12 00:18:53 +0300

    improve

--- kanak_github_connector/models/__init__.py
+++ kanak_github_connector/models/__init__.py
@@ -1,3 +1,12 @@
 # -*- coding: utf-8 -*-
-from . import product
-from . import github_repository
+
+# Import modules explicitly at the top of the file
+import product
+import github_repository
+
+# Use absolute imports for better clarity and to avoid potential issues
+# with relative imports
+# See: https://www.python.org/dev/peps/pep-0008/#imports
+
+# Consider using a namespace package to group related modules together
+# See: https://packaging.python.org/guides/packaging-namespace-packages/

19006bf81302275958d93e3c8b6b93ce2dbc817c
Cantay <cantay@gmail.com>
2024-03-12 00:18:58 +0300

    improve

--- website_google_analytics_4/__init__.py
+++ website_google_analytics_4/__init__.py
@@ -1,1 +1,13 @@
-from . import models
+from .models import User, Product  # specify the specific models to be imported
+
+def get_users():
+    return models.User.objects.all()
+
+def get_products():
+    return models.Product.objects.all()
+
+def create_user(name, email):
+    return models.User.objects.create(name=name, email=email)
+
+def create_product(name, price):
+    return models.Product.objects.create(name=name, price=price)

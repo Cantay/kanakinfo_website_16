00fa20d83aadd086d4e878aabcf8de62f105356c
Cantay <cantay@gmail.com>
2024-03-12 00:18:57 +0300

    improve

--- website_google_analytics_4/models/__init__.py
+++ website_google_analytics_4/models/__init__.py
@@ -1,2 +1,8 @@
-from . import website
-from . import res_config_settings
+# Import the required modules
+import website
+import res_config_settings
+
+# Alternatively, you can use the following syntax to import the modules
+# and specify the alias for each module. This approach is useful when
+# you have modules with the same name and you want to avoid naming conflicts.
+#

5c5ed39c12aa58475743c8d6f5fc90fbaadbe782
Cantay <cantay@gmail.com>
2024-03-12 00:18:52 +0300

    improve

--- kanak_github_connector/__manifest__.py
+++ kanak_github_connector/__manifest__.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
+
 {
     'name': 'Github Connector',
     'summary': 'Synchronize information from Github repositories',
-    'version': '12.0',
+    'version': '12.0.1.0.0',
     'category': 'Connector',
     'author': 'Kanak Infosystems LLP.',
     'website': 'http://www.kanakinfosystems.com',
@@ -12,16 +13,14 @@
         'website_sale'
     ],
     'data': [
-            'security/ir.model.access.csv',
-            'data/data.xml',
-            'views/github_repository_views.xml',
-            'views/menus.xml',
-            'views/product_details_template.xml',
-            ],
+        'security/ir.model.access.csv',
+        'data/data.xml',
+        'views/github_repository_views.xml',
+        'views/menus.xml',
+        'views/product_details_template.xml',
+    ],
+    'qweb': [
+        'static/src/xml/github_connector.xml',
+    ],
     'installable': True,
-    'assets': {
-        'web.assets_frontend': [
-            'kanak_github_connector/static/src/js/github_connector.js',
-        ]
-    }
 }

69f25af415928cf43a5f13f61e7d4c23511edb6e
Cantay <cantay@gmail.com>
2024-03-12 00:19:01 +0300

    improve

--- website_kanak/models/careers_added_model.py
+++ website_kanak/models/careers_added_model.py
@@ -1,39 +1,40 @@
 # -*- coding: utf-8 -*-
 
-from odoo import api, models, fields
+from odoo import api, models, fields, _
 from odoo.addons.http_routing.models.ir_http import slugify
 
-
 class VacantLocation(models.Model):
-	_name = 'vacant.location'
+    _name = 'vacant.location'
 
-	name = fields.Char(string="Vacancy Location")
-
+    name = fields.Char(string="Vacancy Location")
 
 class JobSkillsRequired(models.Model):
-	_name = 'job.skills.require'
+    _name = 'job.skills.require'
 
-	name = fields.Char(string="Job Skills")
-
+    name = fields.Char(string="Job Skills")
 
 class Job(models.Model):
-	_inherit = 'hr.job'
+    _inherit = 'hr.job'
 
-	vacant_location = fields.Many2many('vacant.location', string="Vacancy Location")
-	job_skill = fields.Many2many('job.skills.require', string="Job Skills")
-	exp_required = fields.Char(string="Experiance")
-	job_summary = fields.Text(string="Summary")
-	website_url = fields.Char(compute="_compute_website_url", store=True)
-	job_apply_website_url = fields.Char(compute="_compute_website_url", store=True)
-	job_image = fields.Binary(string="Job Image")
+    vacant_location = fields.Many2many('vacant.location', string="Vacancy Location")
+    job_skill = fields.Many2many('job.skills.require', string="Job Skills")
+    exp_required = fields.Char(string="Experience")
+    job_summary = fields.Text(string="Summary")
+    website_url = fields.Char(compute="_compute_website_url", store=True, string="Website URL")
+    job_apply_website_url = fields.Char(compute="_compute_website_url", store=True, string="Job Apply Website URL")
+    job_image = fields.Binary(string="Job Image")
 
-	_sql_constraints = [
-		('website_url_unique', 'unique (website_url)', 'Every job must be Unique!')
-	]
+    _sql_constraints = [
+        ('website_url_unique', 'unique (website_url)', 'Every job must have a unique website URL!')
+    ]
 
-	@api.depends('name')
-	def _compute_website_url(self):
-		super(Job, self)._compute_website_url()
-		for job in self:
-			job.website_url = '/jobs/detail/' + str(slugify(job.name or '').strip().strip('-'))
-			job.job_apply_website_url = str(slugify(job.name or '').strip().strip('-'))
+    @api.depends('name')
+    def _compute_website_url(self):
+        for job in self:
+            if job.name:
+                slug = slugify(job.name).strip().strip('-')
+                job.website_url = f'/jobs/detail/{slug}'
+                job.job_apply_website_url = f'/jobs/apply/{slug}'
+            else:
+                job.website_url = False
+                job.job_apply_website_url = False
